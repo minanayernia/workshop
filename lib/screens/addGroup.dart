92a50022486812d7603f4524992c73f28cc2f6f5
minanayernia <minanayernia@gmail.com>
2020-01-14 12:22:09 +0330

    start addgroup

---
+++ lib/screens/addGroup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddGroupPage extends StatefulWidget {
+  @override
+  _AddGroupPageState createState() => _AddGroupPageState();
+}
+
+class _AddGroupPageState extends State<AddGroupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

55c709718a6200695a7f79b9e52dd8c260661f54
baharjenabzade <baharjbz@gmail.com>
2020-01-19 16:04:29 +0330

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,31 +1,32 @@
 // import 'dart:js';
 
 import 'package:image_picker/image_picker.dart';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:workshop/widgets/profileCard.dart';
 import 'package:workshop/widgets/topbar.dart';
 // import 'package:workshop/screens/splash.dart';
-
+import 'package:http/http.dart' as http;
 
 class CustomPopupMenu {
   CustomPopupMenu({this.title, this.icon});
- 
+
   String title;
   IconData icon;
 }
+
 List<CustomPopupMenu> choices = <CustomPopupMenu>[
   CustomPopupMenu(title: 'Home', icon: Icons.home),
   CustomPopupMenu(title: 'Bookmarks', icon: Icons.bookmark),
   CustomPopupMenu(title: 'Settings', icon: Icons.settings),
 ];
 
-
 class SelectedOption extends StatelessWidget {
   CustomPopupMenu choice;
- 
+
   SelectedOption({Key key, this.choice}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,102 +56,96 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      
       body: Center(
         child: Column(
           children: <Widget>[
             // TopBar(foo: "Profile",),
             ProfileAppBar(),
             Container(
-              height:MediaQuery.of(context).size.height-140,
-              width: MediaQuery.of(context).size.width*0.95,
-              child: SingleChildScrollView(child: ProCard())),
+                height: MediaQuery.of(context).size.height - 140,
+                width: MediaQuery.of(context).size.width * 0.95,
+                child: SingleChildScrollView(child: ProCard())),
           ],
         ),
       ),
     );
   }
 }
+
 enum WhyFarther { harder, smarter, selfStarter, tradingCharter }
 
 class ProfileAppBar extends StatefulWidget {
-
   @override
   _ProfileAppBarState createState() => _ProfileAppBarState();
 }
 
 class _ProfileAppBarState extends State<ProfileAppBar> {
-    String menu = "...";
-    
-    
+  String menu = "...";
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 10 , bottom: 8),
+      margin: EdgeInsets.only(top: 10, bottom: 8),
       height: 50,
-      width: MediaQuery.of(context).size.width*0.9,
-      decoration: BoxDecoration(color: Colors.white , borderRadius: BorderRadius.circular(25)),
-      child: Center(child: Row(
+      width: MediaQuery.of(context).size.width * 0.9,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(25)),
+      child: Center(
+          child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 20),
-                  height: 30,
-                  // width:MediaQuery.of(context).size.width*0.8 ,
-                  child: Text("Profile" , style: TextStyle(color: Colors.deepPurple , fontSize: 20),)),
-                  // PopupMenuButton<WhyFarther>(itemBuilder: (context,),
-                    
-                  // ),
-                GestureDetector(
-                  onTap: (){
-                    setState(() {
-                      if(menu == "..." ){
-                        menu = "X" ;
-                      }else{
-                        menu = "...";
-                      }
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(left: 20),
+              height: 30,
+              // width:MediaQuery.of(context).size.width*0.8 ,
+              child: Text(
+                "Profile",
+                style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+              )),
+          // PopupMenuButton<WhyFarther>(itemBuilder: (context,),
 
-          //             PopupMenuButton<CustomPopupMenu>(
-          //   elevation: 3.2,
-          //   initialValue: choices[1],
-          //   onCanceled: () {
-          //     print('You have not chossed anything');
-          //   },
-          //   tooltip: 'This is tooltip',
-          //   onSelected: _select,
-          //   itemBuilder: (BuildContext context) {
-          //     return choices.map((CustomPopupMenu choice) {
-          //       return PopupMenuItem<CustomPopupMenu>(
-          //         value: choice,
-          //         child: Text(choice.title),
-          //       );
-          //     }).toList();
-          //   },
-          // )
+          // ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                if (menu == "...") {
+                  menu = "X";
+                } else {
+                  menu = "...";
+                }
 
-
-
-                    });
-
-                  },
-                    child: Container(
-                    height: 30,
-                    width: 30,
-                    child: Text("..."),),
-                )
-              ],
+                //             PopupMenuButton<CustomPopupMenu>(
+                //   elevation: 3.2,
+                //   initialValue: choices[1],
+                //   onCanceled: () {
+                //     print('You have not chossed anything');
+                //   },
+                //   tooltip: 'This is tooltip',
+                //   onSelected: _select,
+                //   itemBuilder: (BuildContext context) {
+                //     return choices.map((CustomPopupMenu choice) {
+                //       return PopupMenuItem<CustomPopupMenu>(
+                //         value: choice,
+                //         child: Text(choice.title),
+                //       );
+                //     }).toList();
+                //   },
+                // )
+              });
+            },
+            child: Container(
+              height: 30,
+              width: 30,
+              child: Text("..."),
+            ),
+          )
+        ],
       )),
-      
-      
     );
   }
 }
 
-
-
-
-class MinaAppBar extends StatelessWidget implements PreferredSizeWidget{
-
+class MinaAppBar extends StatelessWidget implements PreferredSizeWidget {
   final double height;
 
   const MinaAppBar({
@@ -161,42 +156,43 @@ class MinaAppBar extends StatelessWidget implements PreferredSizeWidget{
   @override
   Size get preferredSize => Size.fromHeight(height);
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      height: 100,
-          child: Center(child:
-        //     child: Container(
-        
-        // height: 50,
-        // width: MediaQuery.of(context).size.width * 0.9,
-        // decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
-        //  color: Colors.yellow
-        //  ),
-        // child: 
-        Container(
-          height: 20,
-                  child: AppBar(
-            backgroundColor: Colors.green,
-            
-            title: Container(child: Text('PROFILE'),),
-            
-            automaticallyImplyLeading: false,
-            shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-            top: Radius.circular(200),
-            bottom: Radius.circular(200),
-      ),
+        color: Colors.red,
+        height: 100,
+        child: Center(
+          child:
+              //     child: Container(
+
+              // height: 50,
+              // width: MediaQuery.of(context).size.width * 0.9,
+              // decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+              //  color: Colors.yellow
+              //  ),
+              // child:
+              Container(
+            height: 20,
+            child: AppBar(
+              backgroundColor: Colors.green,
+
+              title: Container(
+                child: Text('PROFILE'),
+              ),
+
+              automaticallyImplyLeading: false,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(200),
+                  bottom: Radius.circular(200),
+                ),
+              ),
+              // ),
             ),
-            // ),
-            ),
-        ),)
-    );
+          ),
+        ));
   }
 }
-
 
 class PopupMenuButton extends StatefulWidget {
   @override
@@ -206,8 +202,17 @@ class PopupMenuButton extends StatefulWidget {
 class _PopupMenuButtonState extends State<PopupMenuButton> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
+  }
+}
+
+void get_req() async {
+  Future<http.Response> sendPersonDetail() async {
+    var response = await http.get('http://192.168.43.139:8080/api/v1/signup',
+        headers: {
+          "Authorization" : "Bearer " + "",
+          "Accept": "application/json",
+          "content-type": "application/json"
+        });
   }
 }
